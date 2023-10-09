514b25ef0e53836f1bcad51c6d5049a6e8011449
Niranjan0107 <amitniranjan9@gmail.com>
2023-10-10 01:47:41 +0530

    Homepage added

--- assets/js/global.js
+++ assets/js/global.js
@@ -55,6 +55,35 @@ var lightboxDescription = GLightbox({
      autoplaySpeed: 1500,
      slidesToShow: 2,
      infinite: true,
+     responsive: [
+       {
+         breakpoint: 1024,
+         settings: {
+           slidesToShow: 3,
+           slidesToScroll: 3,
+           infinite: true,
+           dots: true
+         }
+       },
+       {
+         breakpoint: 600,
+         settings: {
+           slidesToShow: 2,
+           slidesToScroll: 2
+         }
+       },
+       {
+         breakpoint: 480,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1
+         }
+       }
+       // You can unslick at a given breakpoint now by adding:
+       // settings: "unslick"
+       // instead of a settings object
+     ]
+     
    });
  });
 
@@ -69,5 +98,38 @@ var lightboxDescription = GLightbox({
      autoplaySpeed: 3500,
      slidesToShow: 3,
      infinite: true,
+     responsive: [
+       {
+         breakpoint: 1024,
+         settings: {
+           slidesToShow: 3,
+           slidesToScroll: 3,
+           infinite: true,
+           dots: true
+         }
+       },
+       {
+         breakpoint: 600,
+         settings: {
+           slidesToShow: 2,
+           slidesToScroll: 2
+         }
+       },
+       {
+         breakpoint: 480,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1
+         }
+       }
+       // You can unslick at a given breakpoint now by adding:
+       // settings: "unslick"
+       // instead of a settings object
+     ]
    });
+ });
+
+
+ $(".mbl-search-box").click(function(){
+   $(".mbl-search-form").toggleClass("active");
  });
